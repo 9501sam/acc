90b607233bcca53b63b52ea42e2b818544864fbf
RickCompileError <gngn15401@gmail.com>
2022-12-18 23:03:33 +0800

    edit main function and add file_exists function

--- sbcc.c
+++ sbcc.c
@@ -4,11 +4,28 @@
 #include "scanner.h"
 #include "parser.h"
 
+bool file_exist(char *path){
+	FILE *pfile = fopen(path, "r");
+	bool is_exist = false;
+	if (pfile != NULL)
+		is_exist = true;
+	fclose(path);
+	return is_exist;
+
 int main(int argc, char **argv)
 {
     // TODO: read from file argv[1]
 
-    token_t *tokens = scanning("f b i a a = 5 b = a + 3.2 p b");
+	if (argc == 0){
+		fprintf("Doesn't input any file\n");
+		return -1;
+	}
+
+	for (int i = 0; i < argc; i++){
+		if (file_exist(argv[i]))
+			continue;
+		scanning(argv[i]);
+	}
 
     return 0;
 }
