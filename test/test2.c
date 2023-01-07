68a07e4b9c1564f652285c196df46cb12a9d8766
RickCompileError <gngn15401@gmail.com>
2023-01-08 02:34:54 +0800

    add some test cases

--- test/test2.c
+++ test/test2.c
@@ -9,6 +9,38 @@
  *
  *
  */
-++ -- + - * / % ^ & | =
-ads_qwe123
+
+>= <= == != > <
+
+++                                                                     								-- + - * / % ^ & | =
+
+include
+main
+char
+int
+float
+if
+else
+elseif
+for
+while
+do
+return
+switch
+case
+printf
+scanf
+
+[	]      (			)  {         	}
+
+float ads_qwe123
 int asd_+.
+ad_ jsaidfjiasjdf123123 >< !@_#&!*^@$_
+ad_            																	
+
+,;:#"'
+
+' ' '@' '	' 'a' '1'
+'
+'
+
